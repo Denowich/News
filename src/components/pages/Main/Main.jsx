a28edb65d723e8ddb9b909b49975b615072d606c
Den <denowich@gmail.com>
2026-09-03 13:55:07 +0300

    Adding axios, created component api, into Main.js added useEffect

--- src/components/pages/Main/Main.jsx
+++ src/components/pages/Main/Main.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
 import { NewsBanner } from '../../NewsBanner/NewsBanner';
 import styles from './styles.module.css';
+import { getNews } from '../../api/apiNews';
 
 export const Main = () => {
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const news = await getNews();
+        console.log(news);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchNews();
+  }, []);
+
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>Main</h1>
